b3bdef9963b8f403ae4e40cfebd8085639af68b3
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-14 13:37:55 -0700

    Add: Logic to change square blue on mouse hover

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create square divs
 const container = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
   const row = document.createElement("div");
@@ -9,3 +10,12 @@ for (let i = 0; i < 16; i++) {
   }
   container.append(row);
 }
+
+// Create hover effect
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  // and for each one we add a 'click' listener
+  square.addEventListener("mouseenter", (e) => {
+  e.target.style.background = 'blue';
+  });
+});
